3a7418113a1c6e71ba57fa3c324a4520a78995f9
riyadhswe <riyadhswe@gmail.com>
2021-12-08 20:36:09 +0600

    1st exercise

---
+++ 01 python-example/list1.py
@@ -0,0 +1,6 @@
+list1 = [1, 2, 3, 4, 5]
+for i in list1:
+    print(i)
+    if i==4:
+       break
+print("End of for loop")
---
+++ 01 python-example/script.py
@@ -0,0 +1,5 @@
+name = "Andrew Venis"
+branch = "Computer Science"
+age = "25"
+print("My name is: ", name, )
+print("My age is: ", age)
