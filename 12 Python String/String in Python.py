f3c113d84f812c7ebd7057a5b84836decec8b35b
riyadhswe <riyadhswe@gmail.com>
2021-12-13 19:35:27 +0600

    Python string

--- 12 Python String/String in Python.py
+++ 12 Python String/String in Python.py
@@ -1,2 +1,12 @@
+# Using single quotes
 str1 = 'Hello Python'
-print(str1)  
+print(str1)
+# Using double quotes
+str2 = "Hello Python"
+print(str2)
+
+# Using triple quotes
+str3 = '''R******
+                 I*******       
+              *********Y'''
+print(str3)
