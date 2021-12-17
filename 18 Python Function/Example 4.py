bffb5a478651d393625793b087d3448d4d39a700
riyadhswe <riyadhswe@gmail.com>
2021-12-17 20:26:41 +0600

    Python Built-in Functions

---
+++ 18 Python Function/Example 4.py
@@ -0,0 +1,6 @@
+def sum(x,y):
+    z = x+y
+    return z
+a = int(input(" Please enter a : "))
+b = int(input(" Please enter b : "))
+print(sum(a,b))
---
+++ 18 Python Function/Function.py
@@ -0,0 +1,5 @@
+#function definition  
+def hello_world():    
+    print("hello world")    
+# function calling  
+hello_world()   
